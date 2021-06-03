f59a8b3a40de068668e7b0b71544dc11d8ebfb43
hjq <hjqi98@126.com>
2021-06-03 10:55:01 +0800

    Initial commit

---
+++ src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "DEMO")
+@RestController
+public class DemoController {
+
+    @ApiImplicitParam(name = "name",value = "姓名",required = true)
+    @ApiOperation(value = "WHO U R")
+    @GetMapping("/sayHi")
+    public ResponseEntity<String> sayHi(@RequestParam(value = "name")String name){
+        return ResponseEntity.ok("Hi:"+name);
+    }
+}
